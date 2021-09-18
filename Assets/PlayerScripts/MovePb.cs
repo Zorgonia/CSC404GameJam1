b65e4d2b5af4728de35e79b7bca1a005552bcc20
Zifeng Zhu <zzf741523@gmail.com>
2021-09-18 13:55:37 -0400

    changed the name

---
+++ Assets/PlayerScripts/MovePb.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePb : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+
+    private float _playerInput;
+    private float _turningInput;
+
+    private const float MovementScale = 3.0f;
+    private const float TurningScale = 1.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerInput = Input.GetAxis("Vertical");
+        _turningInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _transform.TransformVector(_turningInput * TurningScale, 0,
+            _playerInput * MovementScale);
+    }
+}
